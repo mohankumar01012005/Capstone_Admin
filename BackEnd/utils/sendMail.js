d7e9c8dadce0939d173d6b20baacf65e0fc1f922
mohankumar01012005 <kumar.mohan@kalvium.community>
2024-04-10 11:15:38 +0530

    created a branch called mailUpdated and updated the mail component part

--- BackEnd/utils/sendMail.js
+++ BackEnd/utils/sendMail.js
@@ -12,9 +12,9 @@ const sendMail = async (userName, name) => {
   let info = await transporter.sendMail({
     from: '"Mohan kumar 👻" <mohantheboss1432@gmail.com>',
     to: userName,
-    subject: name,
-    text: name,
-    html: `<p>Hello User you have berrn</p>`,
+    subject: "Successful verification",
+    text: "Verification Successful and You are Ready to explore our Website",
+    html: `<p>Congradulations  ${name} your request for creating account in our website CricElevate you have been verified and noe you are ready to Sign In. All the very Best</p>`,
   });
   console.log("Message sent: %s", info.messageId);
   // res.json({info});
